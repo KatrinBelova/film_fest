710ee5bf066d4b60b7295dfc8b3add7d2f3c98a0
Katya Belova <katrin.belova@gmail.com>
2018-04-12 17:45:55 +0300

    fixed menu, Sections contacts and partners

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
 const $ = window.$ = window.jQuery = require('jquery');
 
+import'../node_modules/fontawesome-markers/fontawesome-markers.min.js'
+
 import './components/header/header';
+import './components/contacts/map';
