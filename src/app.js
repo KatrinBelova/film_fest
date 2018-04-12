50cfbcfbc650fdd517c6a701c7333aa2f17a49c2
Katya Belova <katrin.belova@gmail.com>
2018-04-12 21:39:10 +0300

    Missed semicolon.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const $ = window.$ = window.jQuery = require('jquery');
 
-import'../node_modules/fontawesome-markers/fontawesome-markers.min.js'
+import'../node_modules/fontawesome-markers/fontawesome-markers.min.js';
 
 import './components/header/header';
 import './components/contacts/map';
