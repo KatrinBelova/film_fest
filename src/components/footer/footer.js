f2fbc6d8e91d6c20f669b78eedc5b5c9d843f758
Katya Belova <katrin.belova@gmail.com>
2018-04-13 16:55:47 +0300

    fucking logo.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,14 +1,16 @@
 import $ from 'jquery';
 
-let target = $('.footer__logo');
+let target = $('.footer__logo img');
 let targetPos = target.offset().top;
 let winHeight = $(window).height();
 let scrollToElem = targetPos - winHeight;
 let windowWidth = $(window).width();
+
 $(window).scroll(function(){
+
 	let winScrollTop = $(this).scrollTop();
+
 	if(winScrollTop > scrollToElem && windowWidth > 480) {
-		target.empty();
-		target.append('<img class="footer__gif" src="./images/oxwel_firework.gif" alt="Oxwel logo">')
+		target.replaceWith('<img class="footer__gif" src="./images/oxwel_firework.gif" alt="Oxwel logo">')
 	}
 });
