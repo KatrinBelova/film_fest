979dc26fcc0d00b89a7a355e85597237995135c1
Katya Belova <katrin.belova@gmail.com>
2018-04-13 15:13:15 +0300

    Section Footer, fixed marker map

--- src/components/contacts/map.js
+++ src/components/contacts/map.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 
 window.initMap = function() {
-    var location = { lat: 50.505799, lng: 30.618057};
+    var location = { lat: 50.505791, lng: 30.618063};
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
@@ -10,10 +10,6 @@ window.initMap = function() {
 
         // The latitude and longitude to center the map (always required)
         center: location,
-
-        // How you would like to style the map. 
-        // This is where you would paste any style found on Snazzy Maps.
-        // styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
     };
 
     // Get the HTML DOM element that will contain your map 
@@ -23,30 +19,22 @@ window.initMap = function() {
     // Create the Google Map using our element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
 
+    var icon = {
+        path: "M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z",
+        fillColor: '#c81100',
+        fillOpacity: 1,
+        strokeWeight: 0,
+        scale: 0.12
+    };
     // Let's also add a marker while we're at it
     var marker = new google.maps.Marker({
         map: map,
-        icon: {
-            path: fontawesome.markers.MAP_MARKET_ALT,
-            scale: 0.5,
-            strokeWeight: 0.2,
-            strokeColor: 'black',
-            strokeOpacity: 1,
-            fillColor: '#c81100',
-            fillOpacity: 1
-        },
-        clickable: false,
-        position: new google.maps.LatLng(50.505799, 30.618057)
+        icon: icon,
+        position: new google.maps.LatLng(50.506235, 30.617830)
     });
 
 };
 
-// google.maps.event.addDomListener(window, 'load', initMap); 
 
-// var map;
-//     function initMap() {
-//       map = new google.maps.Map(document.getElementById('map'), {
-//         center: {lat: -34.397, lng: 150.644},
-//         zoom: 8
-//       });
-//     }
+google.maps.event.addDomListener(window, 'load', initMap); 
+
