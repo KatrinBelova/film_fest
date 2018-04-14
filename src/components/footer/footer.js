badb38034a0abacd6a1f03aed6b400cee6a13cda
Katya Belova <katrin.belova@gmail.com>
2018-04-14 11:29:04 +0300

    Fixing footer.js.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,16 +1,14 @@
 import $ from 'jquery';
 
-let target = $('.footer__logo img');
-let targetPos = target.offset().top;
-let winHeight = $(window).height();
-let scrollToElem = targetPos - winHeight;
+let target = $('.footer__img');
+let targetPosition = target.offset().top;
 let windowWidth = $(window).width();
 
 $(window).scroll(function(){
 
 	let winScrollTop = $(this).scrollTop();
 
-	if(winScrollTop > scrollToElem && windowWidth > 480) {
+	if($(document).scrollTop() > targetPosition && windowWidth > 480) {
 		target.replaceWith('<img class="footer__gif" src="./images/oxwel_firework.gif" alt="Oxwel logo">')
 	}
 });
