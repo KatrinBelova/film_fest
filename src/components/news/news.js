58b700425c25fec9db566259e70abdd537264c8c
Artem Verbat <artem.verbat@gmail.com>
2018-04-16 02:43:15 +0300

    refactor: Refactored feature - Disabled slick-carousel on display max-width 992px. Refactored GoogleMapApi load and usage.

--- src/components/news/news.js
+++ src/components/news/news.js
@@ -1,95 +1,94 @@
 import $ from 'jquery';
 
-$(document).ready(function(){
 
-	let windowWidth = $(window).width();
-	let items = $('.news__item');
+let items = $('.news__item');
+//Change opacity, when items more than 4
+let slickTrack = $('.slick-track');
+let active = $('.slick-active');
 
-	if($(items).length > 4 && windowWidth > 992) {
+let $first = active[0];
+let $second = active[1];
+let $third = active[2];
+let $fourth = active[3];
 
-			$('.news__list').slick({
-				slidesToShow: 4
-			});
-	} 
-	
-	//Change opacity, when items more than 4
-	let slickTrack = $('.slick-track');
-	let active = $('.slick-active');
-
-	let $first = active[0];
-	let $second = active[1];
-	let $third = active[2];
-	let $fourth = active[3];
-
-	if($(items).length > 4) {
-
-		$($first).css('opacity','0.5');
-		$($fourth).css('opacity','0.5');
-	};
-
-	$('.slick-prev').on('click', function() {
-		slickTrack = $('.slick-track');
-		active = $('.slick-active');
-
-		$first = active[0];
-		$second = active[1];
-		$third = active[2];
-		$fourth = active[3];
-
-		$($first).css('opacity','0.5');
-		$($fourth).css('opacity','0.5');
-		$($second).css('opacity','1');
-		$($third).css('opacity','1');
-
+function slickify() {
+	$('.news__list').slick({
+		slidesToShow: 4,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: "unslick"
+			}
+        ]
 	});
+}
 
-	$('.slick-next').on('click', function() {
-		slickTrack = $('.slick-track');
-		active = $('.slick-active');
-
-		$first = active[0];
-		$second = active[1];
-		$third = active[2];
-		$fourth = active[3];
-
-		$($first).css('opacity','0.5');
-		$($fourth).css('opacity','0.5');
-		$($second).css('opacity','1');
-		$($third).css('opacity','1');
-
-	});
-
-	//Load more button
-
-	let buttonLoadMore = $('.news__button.button');
-
-	if($(items).length > 4 && $(window).width() <= 992) {
-
-		//Show button Load more, when Items more than 4
-			$(buttonLoadMore).css('display','block');
+if ($(items).length > 4) {
+	slickify();
 	
-			console.log($(items).slice(0,3));
+	$(window).resize(function() {
+		if ($(window).width() > 992) {
+			slickify();
+		}
+	});
+}
 
+if($(items).length > 4) {
+	$($first).css('opacity','0.5');
+	$($fourth).css('opacity','0.5');
+};
 
-		$(function(){
-			let lastItem = ($(items).length - 1);
+$('.slick-prev').on('click', function() {
+	slickTrack = $('.slick-track');
+	active = $('.slick-active');
 
-			$(items).slice(0,3).show();
-			$(buttonLoadMore).on('click', function(e){
+	$first = active[0];
+	$second = active[1];
+	$third = active[2];
+	$fourth = active[3];
 
+	$($first).css('opacity','0.5');
+	$($fourth).css('opacity','0.5');
+	$($second).css('opacity','1');
+	$($third).css('opacity','1');
 
-				e.preventDefault();
-				$('.news__item:hidden').slice(0,lastItem).slideDown("linear");
-
-				if ($('.news__item:hidden').length == 0) {
-
-					$(this).fadeOut('slow')
-				};
-			});
-		});
-
-	} else {
-		$('.news__item').css('display', 'block');
-		$(buttonLoadMore).css('display','none');
-	};
 });
+
+$('.slick-next').on('click', function() {
+	slickTrack = $('.slick-track');
+	active = $('.slick-active');
+
+	$first = active[0];
+	$second = active[1];
+	$third = active[2];
+	$fourth = active[3];
+
+	$($first).css('opacity','0.5');
+	$($second).css('opacity','1');
+	$($third).css('opacity','1');
+	$($fourth).css('opacity','0.5');
+});
+
+
+let buttonLoadMore = $('.news__button');
+
+if ($(items).length > 4 && $(window).width() <= 992) {
+
+	$(buttonLoadMore).css('display','block');
+
+	$(function(){
+		let lastItem = ($(items).length - 1);
+
+		$(items).slice(0, 3).show();
+
+		$(buttonLoadMore).on('click', function(e) {
+			e.preventDefault();
+
+			$('.news__item:hidden').slice(0,lastItem).slideDown("linear");
+
+			if ($('.news__item:hidden').length == 0) {
+				$(this).fadeOut('slow');
+			};
+		});
+	});
+}
