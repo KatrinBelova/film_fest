314d88eca74dfa4285e125abba0d4efa3e01cbd9
Katya Belova <katrin.belova@gmail.com>
2018-04-16 17:43:33 +0300

    Adaptive program.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import 'slick-carousel';
 
 import './components/header/header';
 import './components/news/news';
+import './components/program/program';
 import './components/nominees/nominees';
 import './components/gallery/gallery';
 import './components/contacts/map';
---
+++ src/components/program/program.js
@@ -0,0 +1,48 @@
+import $ from 'jquery';
+
+let links = $('.program__title');
+let linksArray = $.makeArray(links);
+let allEvents = $('.program__event');
+
+$(window).resize(function() {
+
+	// let links = $('.program__title');
+	// let linksArray = $.makeArray(links);
+	// let allEvents = $('.program__event');
+
+	if ($(window).width() < 992) {
+
+		for(let i = 0; i < (linksArray.length); i++){
+
+			let link = linksArray[i];
+			let isLinkEmpty = $(link).text().toString();
+			let parent = $(link).parent();
+
+			if(isLinkEmpty == ""){
+				$(parent).fadeOut();
+			}
+		}	
+	} else {
+		$(allEvents).fadeIn();
+	}
+
+});
+
+
+
+if ($(window).width() < 992) {
+	
+
+		for(let i = 0; i < (linksArray.length); i++){
+
+			let link = linksArray[i];
+			let isLinkEmpty = $(link).text().toString();
+			let parent = $(link).parent();
+
+			if(isLinkEmpty == ""){
+				$(parent).fadeOut();
+			}
+		}	
+} else {
+	$(allEvents).fadeIn();
+};
